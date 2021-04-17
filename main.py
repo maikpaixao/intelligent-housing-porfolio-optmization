f674e9c4d277bbe066f042c19cd267662164f800
maik.paixao <paixomaik@outlook.com.br>
2021-04-16 21:03:05 -0300

    graphs added

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="whitegrid")
 
 listings = pd.read_csv('data/simulated_listings.csv')
 target = pd.read_csv('data/target_apartments.csv')
 
-print(listings.isnull().values.any())
+corrMatrix = listings.corr()
 
+matplotlib.rcParams.update({'font.size': 7})
+
+#sns.heatmap(corrMatrix, annot=True)
+#sns.scatterplot(x=listings['useful_area'], y=listings['value'])
+sns.displot(x=listings['rooms'])
+
+plt.show()
