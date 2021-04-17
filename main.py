f4d451ef84a1cf88a0006d0aebd9efbc516867e9
maik.paixao <paixomaik@outlook.com.br>
2021-04-17 00:32:31 -0300

    regression added

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ import numpy as np
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
+matplotlib.rcParams.update({'font.size': 7})
 sns.set_theme(style="whitegrid")
 
 listings = pd.read_csv('data/simulated_listings.csv')
 target = pd.read_csv('data/target_apartments.csv')
 
+'''
+listings = listings[listings['sold']==1]
+x = listings.iloc[:, :4]
+y = listings['time_on_market']
+'''
+
+#reg = LinearRegression().fit(x, y)
+#print(reg.score(x, y), reg.intercept_)
+
 corrMatrix = listings.corr()
 
-matplotlib.rcParams.update({'font.size': 7})
+#secondary = listings[listings['rooms']==8]
+#print(secondary.head())
 
 #sns.heatmap(corrMatrix, annot=True)
-#sns.scatterplot(x=listings['useful_area'], y=listings['value'])
-sns.displot(x=listings['rooms'])
+sns.scatterplot(x=listings['time_on_market'], y=listings['value'])
+#sns.barplot(x=listings['rooms'], y=listings['time_on_market']) 
 
 plt.show()
