8b3f04b3e94403ddbbd36e3d7a4bbc7ceb6142b5
maik.paixao <paixomaik@outlook.com.br>
2021-04-19 10:13:38 -0300

    changes added

--- main.py
+++ main.py
@@ -5,28 +5,30 @@ import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 sns.set_theme(style="whitegrid")
 
 listings = pd.read_csv('data/simulated_listings.csv')
 target = pd.read_csv('data/target_apartments.csv')
 
-#change coluns: value => interior_quality
-cols = listings.columns.tolist()
-cols = cols[:5] + [cols[6]]  + [cols[5]] + cols[7:]
-listings = listings[cols]
+#print(listings.describe())
 
-'''
-listings = listings[listings['sold']==0]
-#listings = listings[listings['rooms'] < 7]
-#listings = listings[listings['garages'] < 6]
+#listings = listings[listings['sold']==0]
+listings = listings[listings['rooms'] < 7]
+listings = listings[listings['garages'] < 6]
 
+#Standartization
+#scaler = StandardScaler()
+#listings = scaler.fit_transform(listings)
+#listings = pd.DataFrame(listings)
+
+#Correlation matrix
 corrMatrix = listings.corr()
 
 matplotlib.rcParams.update({'font.size': 7})
 #sns.heatmap(corrMatrix, annot=True)
-#sns.scatterplot(x=listings['value'], y=listings['interior_quality'])
-sns.barplot(x=listings['interior_quality'], y=listings['value']) 
+sns.scatterplot(x=listings['value'], y=listings['time_on_market'])
+#sns.barplot(x=listings['interior_quality'], y=listings['value']) 
 
 plt.show()
-'''
